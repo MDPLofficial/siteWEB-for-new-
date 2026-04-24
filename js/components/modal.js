11898d10254529f7737929fa20e0892f1a7fcd88
PorVad <112697838+PorVad@users.noreply.github.com>
2026-04-23 23:25:16 -0400

    Modal logic (open close), opimized css file, added icons on home page

--- js/components/modal.js
+++ js/components/modal.js
@@ -1,3 +1,27 @@
-export async function initModal() {
-  return 0;
+const modal = document.querySelector(".backdrop");
+
+export function initModal() {
+  document.addEventListener("click", (e) => {
+    if (e.target.closest(".become-citizen-btn")) {
+      showModal();
+    }
+
+    if (e.target.closest(".close-btn")) {
+      hideModal();
+    }
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      hideModal();
+    }
+  });
+}
+
+function showModal() {
+  modal.classList.add("is-open");
+}
+
+function hideModal() {
+  modal.classList.remove("is-open");
 }
