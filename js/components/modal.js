a419f648a4a5b5d02f8074a71cc7fcb8395ae33e
PorVad <112697838+PorVad@users.noreply.github.com>
2026-04-28 18:44:32 -0400

    small changes + header modal btn

--- js/components/modal.js
+++ js/components/modal.js
@@ -1,7 +1,11 @@
-const modal = document.querySelector(".backdrop");
+let modal = document.querySelector(".backdrop");
 
 export function initModal() {
   document.addEventListener("click", (e) => {
+    if (!modal) {
+      modal = createModal();
+    }
+
     if (e.target.closest(".become-citizen-btn")) {
       showModal();
     }
@@ -24,4 +28,50 @@ function showModal() {
 
 function hideModal() {
   modal.classList.remove("is-open");
+}
+
+function createModal() {
+  const modal = document.createElement("div");
+  modal.className = "backdrop";
+
+  modal.innerHTML = `
+    <div id="modal" class="modal">
+            <div class="container container-modal">
+                <button id="close-btn" class="close-btn">X</button>
+                <div class="modal-header">
+                    <h2>Стати громадянином</h2>
+                    <p>Подайте заявку на приєднання до МДПЛ</p>
+                </div>
+                <form class="blank">
+                    <label>
+                        Прізвище та Ім'я
+                        <input class="contact-modal-form" type="text" name="name" placeholder="Василь Клеменко">
+                    </label>
+                    <label>
+                        Дата народження
+                        <input class="contact-modal-form"  type="date" name="date">
+                    </label>
+                    <label>
+                        Мета вступу до МДПЛ
+                        <textarea class="contact-modal-form"  name="reason" placeholder="Коротко разкажіть, чому хочете стати частиною держави"></textarea>
+                    </label>
+                    <label>
+                        Ідеї та проєкти
+                        <textarea class="contact-modal-form"  name="projects" placeholder="Над чим ви працюєте або хочете працювати..."></textarea>
+                    </label>
+                    <label>
+                        Бажана посада
+                        <input class="contact-modal-form"  type="text" name="poste" placeholder="Наприклад: Громадянин, Міністр...">
+                    </label>
+                    <div class="modal-btns">
+                        <button class="btn cancle-btn">Скасувати</button>
+                        <button id="submit-btn" class="btn submit-btn">Подати заявку</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+  `;
+
+  document.body.appendChild(modal);
+  return modal;
 }
